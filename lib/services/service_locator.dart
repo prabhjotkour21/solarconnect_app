177529cf6740a901418362fed6d9b07963398a63
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-06 16:23:30 +0530

    feat: add service locator

---
+++ lib/services/service_locator.dart
@@ -0,0 +1,17 @@
+import 'api_service.dart';
+import 'auth_service.dart';
+import 'dashboard_service.dart';
+import 'inverter_service.dart';
+import 'user_profile_service.dart';
+
+class ServiceLocator {
+  ServiceLocator._();
+
+  static final ServiceLocator instance = ServiceLocator._();
+
+  late final ApiService apiService = ApiService();
+  late final AuthService authService = AuthService(apiService);
+  late final InverterService inverterService = InverterService(apiService);
+  late final DashboardService dashboardService = DashboardService(apiService);
+  late final UserProfileService userProfileService = UserProfileService(apiService);
+}
