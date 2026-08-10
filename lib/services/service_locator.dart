bd7bb932e0ffcb0753b136391b683b225885ba91
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-10 10:36:43 +0530

    refactor: register energy service dependencies

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -2,6 +2,7 @@ import 'api_service.dart';
 import 'auth_service.dart';
 import 'dashboard_service.dart';
 import 'device_service.dart';
+import 'energy_service.dart';
 import 'inverter_service.dart';
 import 'user_profile_service.dart';
 
@@ -13,6 +14,7 @@ class ServiceLocator {
   late final ApiService apiService = ApiService();
   late final AuthService authService = AuthService(apiService);
   late final DeviceService deviceService = DeviceService(apiService);
+  late final EnergyService energyService = EnergyService(apiService);
   late final InverterService inverterService = InverterService(apiService);
   late final DashboardService dashboardService = DashboardService(apiService);
   late final UserProfileService userProfileService = UserProfileService(apiService);
