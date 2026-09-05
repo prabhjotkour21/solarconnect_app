1bc455c20513085a5fb5c9247833d145e4b10a4b
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-09-05 17:10:21 +0530

    Update parameter service

--- lib/services/parameter_service.dart
+++ lib/services/parameter_service.dart
@@ -6,4 +6,5 @@ class ParameterService {
 
   Future<Map<String, dynamic>> loadDefinitions(String deviceId, String token) => _apiService.post('/devices/$deviceId/parameters/load', token: token);
   Future<Map<String, dynamic>> getCurrentValues(String deviceId, String token) => _apiService.post('/devices/$deviceId/parameters/current-values', token: token);
+  Future<Map<String, dynamic>> validateParameters(String deviceId, String token, List<Map<String, dynamic>> items) => _apiService.post('/devices/$deviceId/parameters/validate', token: token, body: {'items': items});
 }
